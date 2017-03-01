f292411c992f6f9321d8b70d2d6035351e786510
henyo <1213249791@qq.com>
2017-03-01 19:19:25 +0800

    111

---
+++ html-后台管理/js/script.js
@@ -0,0 +1,28 @@
+$(function(){
+//	if (window.console) {
+//			
+//	
+//	/*触屏*/
+//		$(".slider .inner").bind("swipeleft",function(){
+// 			
+//		});
+//		
+//		$(".slider .inner").bind("swiperight",function(){
+// 			
+//		});
+//		
+//		$('.header .nav li a').hover == $('.header .nav li a').click;
+//		$('.header .nav li').hover() == $('.header .nav li').click();
+		$('.header .nav li').hover(function(){
+			$('.header .nav li').removeClass('selected');
+			$(this).addClass('selected');
+		})
+		
+
+      	
+      	
+
+
+
+})
+
---
+++ html-海天/js/script.js
@@ -0,0 +1,24 @@
+$(function(){
+		
+	
+	/*触屏*/
+		$(".slider .inner").bind("swipeleft",function(){
+ 			
+		});
+		
+		$(".slider .inner").bind("swiperight",function(){
+ 			
+		});
+//		$('.nav li').hover(function(){
+//			$('.nav li').removeClass('selected');
+//			$(this).addClass('selected');
+//		}) 侧边栏导航效果,也可用CSS来做;
+//		
+
+      	
+      	
+
+
+
+})
+
